83ea134b25e7680d0b8784536ac21995c845c903
Pablo Fiorido <pablofiorido@hotmail.com>
2020-09-15 17:03:46 -0300

    Clase 05: board updated to ES6

--- src/components/screens/Game/index.js
+++ src/components/screens/Game/index.js
@@ -1,67 +1,78 @@
 import './style.css';
 import React from 'react';
 
+/*
 function Circle(props) {
+    return (
+        <button className="circle" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
+}
+*/
+
+const Circle = props => {
+    return (
+        <button className="circle" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
+}
+
+const Board = props => {
     return (
         <div className="board">
             <div className="row">
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
             </div>
             <div className="row">
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
             </div>
             <div className="row">
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
             </div>
             <div className="row">
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
             </div>
             <div className="row">
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
             </div>
             <div className="row">
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-                <button className="circle" onClick={props.onClick}>{props.value}</button>
-
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
             </div>
         </div>
     )
 }
 
 
-
-export default Circle;
+export default Board;
