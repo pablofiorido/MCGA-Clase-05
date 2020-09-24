2618a2c15611683e8921e3823e0609de72df221a
Pablo Fiorido <pablofiorido@hotmail.com>
2020-09-24 11:59:53 -0300

    Clase 06: changes made to Products' screen

--- src/components/screens/Products/index.js
+++ src/components/screens/Products/index.js
@@ -2,20 +2,44 @@ import './style.css';
 import React from 'react';
 
 
-function Products() {
-    return (
-        <div className="Products">
-            <h1>Products</h1>
-            <ul>
-                <li>Alfajor</li>
-                <li>Gaseosa</li>
-                <li>Azucar</li>
-                <li>Leche</li>
-                <li>Yerba</li>
-                <li>Mate</li>
-            </ul>
-        </div>
-    );
+class Products extends React.Component {
+
+    constructor(props) {
+
+        super(props)
+        this.state = { users: [] }
+
+    }
+    componentDidMount() {
+        fetch('https://randomuser.me/api/?results=5')
+            .then(response => response.json())
+            .then(data => this.setState({ users: data.results }))
+    }
+
+    renderUsers() {
+        const users = this.state.users
+        return users.map(user => (
+            <li>
+                <h2>{user.name.first} {user.name.last}</h2>
+                <a href={'mailto:' + user.email}>{user.email}</a>
+                <p>Gender: {user.gender}</p>
+                <img src={user.picture.medium} alt="user" />
+            </li>
+        ))
+
+    }
+
+    render() {
+
+        return (
+            <div className="Products">
+                <h1>Products</h1>
+                <ul>
+                    {this.renderUsers()}
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default Products;
